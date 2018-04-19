19c3a28a69c928d98d256d0b73ca6aea88f49d6a
hector.mendoza <hector.mendoza@REFN000R90JDEWT.ac.sep.gob.mx>
2018-04-19 18:09:54 -0500

    autoupdate

--- aulaaprende.js
+++ aulaaprende.js
@@ -262,58 +262,11 @@ if(require("os").userInfo().username=="root") {
 }
 */
 
-
-var AutoUpdater = require('auto-updater');
-
-var autoupdater = new AutoUpdater({
- autoupdate: false,
- checkgit: false,
- progressDebounce: 0,
- devmode: false
+const exec = require('child_process').exec;
+var cmd = exec("npm update -g aulaaprende", {cwd: "/", maxBuffer: 200 * 1024},(error, stdout, stderr) => {
+	if(error) {
+		console.log("Error al actualizar");
+	} else {
+		console.log("Update OK");
+	}
 });
-
-// State the events 
-autoupdater.on('git-clone', function() {
-  console.log("You have a clone of the repository. Use 'git pull' to be up-to-date");
-});
-autoupdater.on('check.up-to-date', function(v) {
-  console.info("You have the latest version: " + v);
-});
-autoupdater.on('check.out-dated', function(v_old, v) {
-  console.warn("Your version is outdated. " + v_old + " of " + v);
-  autoupdater.fire('download-update'); // If autoupdate: false, you'll have to do this manually. 
-  // Maybe ask if the'd like to download the update. 
-});
-autoupdater.on('update.downloaded', function() {
-  console.log("Update downloaded and ready for install");
-  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually. 
-});
-autoupdater.on('update.not-installed', function() {
-  console.log("The Update was already in your folder! It's read for install");
-  autoupdater.fire('extract'); // If autoupdate: false, you'll have to do this manually. 
-});
-autoupdater.on('update.extracted', function() {
-  console.log("Update extracted successfully!");
-  console.warn("RESTART THE APP!");
-});
-autoupdater.on('download.start', function(name) {
-  console.log("Starting downloading: " + name);
-});
-autoupdater.on('download.progress', function(name, perc) {
-  process.stdout.write("Downloading " + perc + "% \033[0G");
-});
-autoupdater.on('download.end', function(name) {
-  console.log("Downloaded " + name);
-});
-autoupdater.on('download.error', function(err) {
-  console.error("Error when downloading: " + err);
-});
-autoupdater.on('end', function() {
-  console.log("The app is ready to function");
-});
-autoupdater.on('error', function(name, e) {
-  console.error(name, e);
-});
-
-// Start checking 
-autoupdater.fire('check');
