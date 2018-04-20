6c6ac6c5c9f9ad3851ad235c8f3fea4a0756b4e7
hector.mendoza <hector.mendoza@REFN000R90JDEWT.ac.sep.gob.mx>
2018-04-19 21:08:00 -0500

    install / uninstall

--- scripts/uninstall.js
+++ scripts/uninstall.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
+
 require('crontab').load(function(err, crontab) {
 	var jobs = crontab.jobs({command:'aulaaprende'});
 	if(jobs.length>0) {
 		crontab.remove({command:'aulaaprende'});
 	}
-	var job = crontab.create("/usr/bin/aulaaprende 2>&1 >"+config_dir + "logs/aula.log", '*/1 * * * *', 'Aula @prende 2.0');
 	crontab.save(function(err, crontab) {
 	});
 	checkUser();
