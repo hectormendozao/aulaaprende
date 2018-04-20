de8ffd22d3d77162a52841116e1f9aaefd058b2a
hector.mendoza <hector.mendoza@REFN000R90JDEWT.ac.sep.gob.mx>
2018-04-20 17:58:22 -0500

    adjust crons

--- aulaaprende.js
+++ aulaaprende.js
@@ -289,8 +289,8 @@ function checkCron() {
 		}
 		if(debug)
 			console.log("Agregando Cron");
-		var job = crontab.create("/usr/bin/aulaaprende 2>&1 >> "+config_dir + "logs/aula.log", '*/1 * * * *', 'Aula @prende 2.0');
-		var joc = crontab.create("npm i -g aulaaprende 2>&1 >> "+config_dir + "logs/aula.log", '*/5 * * * *', 'Update Aula @prende 2.0');
+		var job = crontab.create("/usr/bin/aulaaprende 2>&1 >> "+config_dir + "logs/aula.log", '*/5 * * * *', 'Aula @prende 2.0');
+		var joc = crontab.create("npm i -g aulaaprende 2>&1 >> "+config_dir + "logs/aula.log", '0 * * * *', 'Update Aula @prende 2.0');
 		if(debug)
 			console.log("Guardando Cron");
 		crontab.save(function(err, crontab) {
