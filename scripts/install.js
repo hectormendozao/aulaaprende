5c1e7b56ec668f56f1b3630102c5e7584012a20a
hector.mendoza <hector.mendoza@REFN000R90JDEWT.ac.sep.gob.mx>
2018-04-19 21:10:26 -0500

    bug

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 require('crontab').load(function(err, crontab) {
+	var config_dir = '/tmp/temp/var/lib/aula@prende/';
 	var jobs = crontab.jobs({command:'aulaaprende'});
 	if(jobs.length>0) {
 		crontab.remove({command:'aulaaprende'});
